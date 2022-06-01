11b07812ddf4f20a17158fc7be6ad2beee8bc4a9
Vikkram <codevikky@gmail.com>
2022-06-01 11:53:21 +0800

    edited readMe

--- src/main/java/com/vikkram/dao/UserDaoImpl.java
+++ src/main/java/com/vikkram/dao/UserDaoImpl.java
@@ -6,7 +6,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+
 
 @Repository
 public class UserDaoImpl implements com.vikkram.dao.UserDao {
